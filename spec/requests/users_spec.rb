eaee5c9adc9931d2ab2c12f7826f7f60e315e71f
Danilo <danilodelyima@gmail.com>
2018-07-11 00:27:03 -0300

    post and put passing for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   let!(:users) { create_list(:user, 10) }
+  let(:user) { users.first }
   let(:id) { users.first.id }
 
   describe 'GET /users' do
@@ -36,6 +37,45 @@ RSpec.describe 'Users', type: :request do
       let(:id) { 0 }
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'POST /users' do
+
+    context 'when the request is valid' do
+      before { post '/users', params: user.as_json }
+
+      it 'creates a user' do
+        expect(json['name']).to eq(user.name)
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/users', params: { } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe 'PUT /users/:id' do
+    let(:user) { { name: 'Bob' } }
+
+    context 'when the record exists' do
+      before { put "/users/#{id}", params: user.as_json }
+
+      it 'updates the record' do
+        expect(json['name']).to eq('Bob')
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
       end
     end
   end
